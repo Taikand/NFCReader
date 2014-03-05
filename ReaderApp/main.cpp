fde058f5b1e1077e69fd467ab9f83aa225d0e89c
Taikand <daniel_meluta.stanciu@live.com>
2014-03-05 17:40:16 +0200

    Cleared up the project

---
+++ ReaderApp/main.cpp
@@ -0,0 +1,19 @@
+// ConsoleApplication28.cpp : Defines the entry point for the console application.
+//
+
+#include "pch.h"
+
+#include <iostream>
+#include <winscard.h>
+
+#pragma comment(lib,"WINSCard.lib")
+
+using namespace std;
+
+
+
+int main(void)
+{
+
+ return 0;
+}
